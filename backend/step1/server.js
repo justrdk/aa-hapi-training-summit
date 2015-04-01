05238be6bd7b1a85992f287ab9e8a77515914b3a
evilryu <osman.sist@gmail.com>
2015-04-01 01:27:07 -0600

    start hapiserver with default route

---
+++ backend/step1/server.js
@@ -0,0 +1,18 @@
+var Hapi = require('hapi');
+
+var server = new Hapi.Server();
+server.connection({
+	port: 3000
+});
+
+server.route({
+    method: 'GET',
+    path: '/',
+    handler: function (request, reply) {
+        reply('WASAAAAAAAAAAAAAAAAAAAAAAP');
+    }
+});
+
+server.start(function() {
+	console.log('Server running at:', server.info.uri);
+});
