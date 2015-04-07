5a943c7cdb521bfa823a0883a80df6ba40f0f903
evilryu <osman.sist@gmail.com>
2015-04-06 23:58:39 -0600

    added simple authentication example with hapi auth cookie. TODO: a nice ui with login and protected routes by user session

---
+++ backend/step4/routes.js
@@ -0,0 +1,85 @@
+exports.register = function(server, options, next) {
+
+	var home = function(request, reply) {
+		reply('<html><head><title>Login page</title></head><body><h3>Welcome ' + request.auth.credentials.name + '!</h3><br/><form method="get" action="/logout">' + '<input type="submit" value="Logout">' + '</form></body></html>');
+	};
+
+	var login = function(request, reply) {
+
+		if (request.auth.isAuthenticated) {
+			return reply.redirect('/');
+		}
+
+		var message = '';
+		var account = null;
+
+		if (request.method === 'post') {
+
+			if (!request.payload.username ||
+				!request.payload.password) {
+
+				message = 'Missing username or password';
+			} else {
+				account = users[request.payload.username];
+				if (!account ||
+					account.password !== request.payload.password) {
+
+					message = 'Invalid username or password';
+				}
+			}
+		}
+
+		if (request.method === 'get' ||
+			message) {
+
+			return reply('<html><head><title>Login page</title></head><body>' + (message ? '<h3>' + message + '</h3><br/>' : '') + '<form method="post" action="/login">' + 'Username: <input type="text" name="username"><br>' + 'Password: <input type="password" name="password"><br/>' + '<input type="submit" value="Login"></form></body></html>');
+		}
+
+		request.auth.session.set(account);
+		return reply.redirect('/');
+	};
+
+	var logout = function(request, reply) {
+
+		request.auth.session.clear();
+		return reply.redirect('/');
+	};
+
+	server.route([{
+		method: 'GET',
+		path: '/',
+		config: {
+			handler: home,
+			auth: 'session'
+		}
+	}, {
+		method: ['GET', 'POST'],
+		path: '/login',
+		config: {
+			handler: login,
+			auth: {
+				mode: 'try',
+				strategy: 'session'
+			},
+			plugins: {
+				'hapi-auth-cookie': {
+					redirectTo: false
+				}
+			}
+		}
+	}, {
+		method: 'GET',
+		path: '/logout',
+		config: {
+			handler: logout,
+			auth: 'session'
+		}
+	}]);
+
+	next();
+};
+
+exports.register.attributes = {
+	name: 'test-routes',
+	version: '0.0.1'
+};
---
+++ backend/step4/server.js
@@ -0,0 +1,103 @@
+var Hapi = require('hapi');
+
+var users = {
+	test: {
+		id: 'test',
+		password: 'password',
+		name: 'TEST'
+	}
+};
+
+var home = function(request, reply) {
+
+	reply('<html><head><title>Login page</title></head><body><h3>Welcome ' + request.auth.credentials.name + '!</h3><br/><form method="get" action="/logout">' + '<input type="submit" value="Logout">' + '</form></body></html>');
+};
+
+var login = function(request, reply) {
+
+	if (request.auth.isAuthenticated) {
+		return reply.redirect('/');
+	}
+
+	var message = '';
+	var account = null;
+
+	if (request.method === 'post') {
+
+		if (!request.payload.username ||
+			!request.payload.password) {
+
+			message = 'Missing username or password';
+		} else {
+			account = users[request.payload.username];
+			if (!account ||
+				account.password !== request.payload.password) {
+
+				message = 'Invalid username or password';
+			}
+		}
+	}
+
+	if (request.method === 'get' ||
+		message) {
+
+		return reply('<html><head><title>Login page</title></head><body>' + (message ? '<h3>' + message + '</h3><br/>' : '') + '<form method="post" action="/login">' + 'Username: <input type="text" name="username"><br>' + 'Password: <input type="password" name="password"><br/>' + '<input type="submit" value="Login"></form></body></html>');
+	}
+
+	request.auth.session.set(account);
+	return reply.redirect('/');
+};
+
+var logout = function(request, reply) {
+
+	request.auth.session.clear();
+	return reply.redirect('/');
+};
+
+var server = new Hapi.Server();
+server.connection({
+	port: 8000
+});
+
+server.register(require('hapi-auth-cookie'), function(err) {
+
+	server.auth.strategy('session', 'cookie', {
+		password: 'secret',
+		cookie: 'sid-example',
+		redirectTo: '/login',
+		isSecure: false
+	});
+});
+
+server.route([{
+	method: 'GET',
+	path: '/',
+	config: {
+		handler: home,
+		auth: 'session'
+	}
+}, {
+	method: ['GET', 'POST'],
+	path: '/login',
+	config: {
+		handler: login,
+		auth: {
+			mode: 'try',
+			strategy: 'session'
+		},
+		plugins: {
+			'hapi-auth-cookie': {
+				redirectTo: false
+			}
+		}
+	}
+}, {
+	method: 'GET',
+	path: '/logout',
+	config: {
+		handler: logout,
+		auth: 'session'
+	}
+}]);
+
+server.start();
